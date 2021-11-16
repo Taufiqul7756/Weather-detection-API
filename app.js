f078ef695e2faf813bbebeee7132df26e7cfe957
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-16 17:13:18 +0600

    Got temperature

---
+++ app.js
@@ -0,0 +1,28 @@
+var button = document.querySelector("#button");
+var inputValue = document.querySelector("#inputValue");
+var name = document.querySelector("#name");
+var temp = document.querySelector("#temp");
+var description = document.querySelector(".description");
+const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+
+button.addEventListener("click", function () {
+  const city = inputValue.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      var nameValue = data["name"];
+      var tempValue = data["main"]["temp"];
+      var descValue = data["weather"][0]["description"];
+
+      city.innerText = nameValue;
+      temp.innerText = tempValue;
+      description.innerText = descValue;
+
+      const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+      const imgIcon = document.getElementById("weather-status");
+      imgIcon.setAttribute("src", url);
+    });
+});
