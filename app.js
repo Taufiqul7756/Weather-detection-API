fa7ba47c1f1de95fdafa36b6eb6624b7b19a80c4
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-16 22:52:01 +0600

    Weather detecte properly (API)

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var button = document.querySelector("#button");
 var inputValue = document.querySelector("#inputValue");
-var name = document.querySelector("#name");
+var city = document.querySelector("#city");
 var temp = document.querySelector("#temp");
-var description = document.querySelector(".description");
+var description = document.querySelector("#description");
 const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
 
-button.addEventListener("click", function () {
-  const city = inputValue.value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+button.addEventListener("click", function (event) {
+  const cityName = inputValue.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
 
   fetch(url)
     .then((res) => res.json())
@@ -16,12 +16,13 @@ button.addEventListener("click", function () {
       var nameValue = data["name"];
       var tempValue = data["main"]["temp"];
       var descValue = data["weather"][0]["description"];
-
       city.innerText = nameValue;
+
       temp.innerText = tempValue;
       description.innerText = descValue;
 
-      const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+      const url = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      console.log(url);
       const imgIcon = document.getElementById("weather-status");
       imgIcon.setAttribute("src", url);
     });
